9c192adc0fccd826514f49ed0187c31e51a01024
aijkoopmans <arend@wcw-eduroam-195-169-174-152.wireless.uva.nl>
2016-11-23 16:20:47 +0100

    Revert "Revert "bash, ruby, ios""
    This reverts commit 5b4b57b1a81d1d7d6ee25e953a16392cdbe09ac7.

---
+++ Sirikit App/Pods/Target Support Files/Pods-UnicornChat/Pods-UnicornChat-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#import <SpeechKit/SpeechKit.h>
+
+#endif
+
+
+FOUNDATION_EXPORT double Pods_UnicornChatVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UnicornChatVersionString[];
+
--- Sirikit App/UnicornCore/UCAccount.m
+++ Sirikit App/UnicornCore/UCAccount.m
@@ -17,30 +17,38 @@
 }
 
 - (NSString *)sendMessage:(NSString *)message toRecipients:(NSArray *)recipients {
+    
+
     //NSLog(message);
     NSString *encoded = [message stringByReplacingOccurrencesOfString:@" " withString:@"+"];
     //NSLog(encoded);
-    
-    // Halting until Alexa answer using synchronous request
-    NSString *request = [@"https://smsdoctors.herokuapp.com/alexa?q=" stringByAppendingString:encoded];
-    NSLog(@"%@", request);
+    // Sending a synchronous message here...
+    NSString *request = [@"https://smsdoctors.herokuapp.com/alexaengels?q=" stringByAppendingString:encoded];
+    //NSString *encoded = [request stringByReplacingOccurancesOfString:@" " withString:@"%20"];
+    NSLog(request);
     NSURL *url1 = [NSURL URLWithString:request];
     NSURLRequest * urlRequest = [NSURLRequest requestWithURL:url1];
     NSURLResponse * response = nil;
     NSError * error = nil;
     NSData * data = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:&response error:&error];
-    NSString *answer = [[NSString alloc] initWithBytes:[data bytes] length:[data length] encoding:NSUTF8StringEncoding];
     
-    /*
+    // Store response
+    //NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
     //Log response to server
+    NSString *answer = [[NSString alloc] initWithBytes:[data bytes] length:[data length] encoding:NSUTF8StringEncoding];
     NSString *length = [NSString stringWithFormat:@"%d", data.length];
-    NSString *serverUrl = [@"https://smsdoctors.herokuapp.com/hackathon?data=" stringByAppendingString:answer];
-    NSURL *url2 = [NSURL URLWithString:serverUrl];
+    //NSLog(length);
+    //NSLog(answer);
+    NSString *aap = [@"https://smsdoctors.herokuapp.com/hackathon?data=" stringByAppendingString:answer];
+    NSURL *url2 = [NSURL URLWithString:aap];
     NSURLRequest * urlRequest2 = [NSURLRequest requestWithURL:url2];
     NSURLResponse * response2 = nil;
-    NSError *error2 = nil;
-    NSData *data2 = [NSURLConnection sendSynchronousRequest:urlRequest2 returningResponse:&response2 error:&error2];
-    */
+    NSError * error2 = nil;
+    NSData * data2 = [NSURLConnection sendSynchronousRequest:urlRequest2 returningResponse:&response2 error:&error2];
+    
+    
+    
     
     return answer;
 }
